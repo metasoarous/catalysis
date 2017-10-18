04f63bec52ebd0c7bc6850b4b9fc31d4346f77c7
Christopher T Small <metasoarous@gmail.com>
2017-10-18 11:25:26 -0700

    Woops; remove compiled js targets

--- resources/public/js/compiled/out/kabel/util.cljs
+++
@@ -1,7 +0,0 @@
-(ns kabel.util)
-
-(defn on-node? []
-  (and (exists? js/process)
-       (exists? js/process.versions)
-       (exists? js/process.versions.node)
-       true))
--- resources/public/js/compiled/out/kabel/util.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.9.542 {}
-goog.provide('kabel.util');
-goog.require('cljs.core');
-kabel.util.on_node_QMARK_ = (function kabel$util$on_node_QMARK_(){
-return (typeof process !== 'undefined') && (typeof process.versions !== 'undefined') && (typeof process.versions.node !== 'undefined') && (true);
-});
-
-//# sourceMappingURL=util.js.map?rel=1506321316525
